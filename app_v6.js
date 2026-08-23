d5acada8ba06f1e7febb54783689a91bb966aeeb
川井太一 <kawaitaichi@kawaitaichinoMacBook-Air.local>
2026-08-24 07:31:54 +0900

    召喚ユニット「菌糸の獣骸」の攻撃・防御ボタンを押した際、対象の与えるダメージを常時-1するデバフ効果を宣言するログが流れるように対応

--- app_v6.js
+++ app_v6.js
@@ -3514,6 +3514,10 @@ function updateUI() {
                     logMsg(`【泥濘の人形の効果】任意の対象全ては、そのターンの「コンボ」を最大2枚までに制限される！<br><span style="color:#aaa; font-size:0.8rem;">（※対象となったプレイヤーは、このラウンドのコンボ枚数を自己管理してください）</span>`, 'important');
                 }
                 
+                if (s.card.name === '菌糸の獣骸') {
+                    logMsg(`【菌糸の獣骸の宣言】このユニットが場に存在する限り、任意の対象全ての与えるダメージを常時「-1」する！`, 'important');
+                }
+                
                 if (s.card.name === '狂雷の凶鳥') {
                     logMsg(`【狂雷の凶鳥の攻撃】このユニットのダメージはリアクションすることができない！`, 'damage');
                 }
@@ -3571,6 +3575,10 @@ function updateUI() {
                 
                 if (s.card.name === '泥濘の人形') {
                     logMsg(`【泥濘の人形の効果】任意の対象全ては、そのターンの「コンボ」を最大2枚までに制限される！<br><span style="color:#aaa; font-size:0.8rem;">（※対象となったプレイヤーは、このラウンドのコンボ枚数を自己管理してください）</span>`, 'important');
+                }
+                
+                if (s.card.name === '菌糸の獣骸') {
+                    logMsg(`【菌糸の獣骸の宣言】このユニットが場に存在する限り、任意の対象全ての与えるダメージを常時「-1」する！`, 'important');
                 }
                 
                 updateUI();
