99997bec8b810b4f53576be59bce969f13da7543
川井太一 <kawaitaichi@kawaitaichinoMacBook-Air.local>
2026-08-24 07:29:53 +0900

    召喚ユニット「泥濘の人形」の攻撃・防御ボタンを押した際、対象のコンボ枚数を制限するデバフ効果を宣言するログが流れるように対応

--- app_v6.js
+++ app_v6.js
@@ -3510,6 +3510,10 @@ function updateUI() {
                     player.deck.draw(1);
                 }
                 
+                if (s.card.name === '泥濘の人形') {
+                    logMsg(`【泥濘の人形の効果】任意の対象全ては、そのターンの「コンボ」を最大2枚までに制限される！<br><span style="color:#aaa; font-size:0.8rem;">（※対象となったプレイヤーは、このラウンドのコンボ枚数を自己管理してください）</span>`, 'important');
+                }
+                
                 if (s.card.name === '狂雷の凶鳥') {
                     logMsg(`【狂雷の凶鳥の攻撃】このユニットのダメージはリアクションすることができない！`, 'damage');
                 }
@@ -3563,6 +3567,10 @@ function updateUI() {
                 if (s.card.name === '浸食の骸樹') {
                     logMsg(`【浸食の骸樹の効果】任意の捨札1枚を廃棄札に移動させる！その後、自身の山札から1枚ドローした！`, 'important');
                     player.deck.draw(1);
+                }
+                
+                if (s.card.name === '泥濘の人形') {
+                    logMsg(`【泥濘の人形の効果】任意の対象全ては、そのターンの「コンボ」を最大2枚までに制限される！<br><span style="color:#aaa; font-size:0.8rem;">（※対象となったプレイヤーは、このラウンドのコンボ枚数を自己管理してください）</span>`, 'important');
                 }
                 
                 updateUI();
