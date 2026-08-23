0582cfa05654398edd4644906e4147a87aec6218
川井太一 <kawaitaichi@kawaitaichinoMacBook-Air.local>
2026-08-24 06:59:20 +0900

    召喚ユニット「狂雷の凶鳥」の攻撃ボタンを押した際の単独宣言ログと、コンボ攻撃時の追撃表示に「[リアクション不可]」のタグが表示されるように対応

--- app_v6.js
+++ app_v6.js
@@ -832,7 +832,7 @@ function setupEvents() {
                     
                     summonDmg += atk;
                     let extraInfo = '';
-                    if (s.card.name === 'サラマンダー' || s.card.name === 'ファントムレオ') {
+                    if (s.card.name === 'サラマンダー' || s.card.name === 'ファントムレオ' || s.card.name === '狂雷の凶鳥') {
                         extraInfo = ' <span style="color:#ffcc00; font-weight:bold;">[リアクション不可]</span>';
                     }
                     if (s.card.name === 'フォレストコング') {
@@ -3470,6 +3470,10 @@ function updateUI() {
                 
                 if (s.card.name === '泥瘴の悪鬼') {
                     logMsg(`【泥瘴の悪鬼の攻撃】任意の対象全ての回収ポイントを-1する！`, 'damage');
+                }
+                
+                if (s.card.name === '狂雷の凶鳥') {
+                    logMsg(`【狂雷の凶鳥の攻撃】このユニットのダメージはリアクションすることができない！`, 'damage');
                 }
                 
                 if (s.card.name === 'テクトニックライノ') {
