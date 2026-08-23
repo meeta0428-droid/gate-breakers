2eeaedd0645e3be9e156116c2881fd4707c1b509
川井太一 <kawaitaichi@kawaitaichinoMacBook-Air.local>
2026-08-24 06:45:36 +0900

    召喚ユニット「フリップサイド・ヒュドラ」がコンボに参加した際、追撃ログに『多頭の暴虐』の効果（全体4ダメージ）を追加表示するように対応

--- app_v6.js
+++ app_v6.js
@@ -841,6 +841,9 @@ function setupEvents() {
                     if (s.card.name === 'ミストストーカー') {
                         extraInfo = ' <span style="color:#ffcc00; font-weight:bold;">[「肉体」リアクション不可]</span>';
                     }
+                    if (s.card.name === 'フリップサイド・ヒュドラ') {
+                        extraInfo = '<br><span style="color:#ffcc00; font-weight:bold;">※『多頭の暴虐』：任意の対象全員に4点のダメージ！</span>';
+                    }
                     summonLog += `・召喚「${s.card.name}」の追撃 (＋${atk})${extraInfo}<br>`;
                 }
             }
