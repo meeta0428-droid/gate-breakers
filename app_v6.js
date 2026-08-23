e586d4e899628abe95325bd92f94e72b15fb3e5d
川井太一 <kawaitaichi@kawaitaichinoMacBook-Air.local>
2026-08-24 06:58:25 +0900

    召喚ユニット「泥瘴の悪鬼」がコンボに参加した際、追撃ログにも「任意の対象全ての回収ポイントを-1する！」というデバフ効果を追加表示するように対応

--- app_v6.js
+++ app_v6.js
@@ -844,6 +844,9 @@ function setupEvents() {
                     if (s.card.name === 'フリップサイド・ヒュドラ') {
                         extraInfo = '<br><span style="color:#ffcc00; font-weight:bold;">※『多頭の暴虐』：任意の対象全員に4点のダメージ！</span>';
                     }
+                    if (s.card.name === '泥瘴の悪鬼') {
+                        extraInfo = '<br><span style="color:#ff5252; font-weight:bold;">※任意の対象全ての回収ポイントを-1する！</span>';
+                    }
                     summonLog += `・召喚「${s.card.name}」の追撃 (＋${atk})${extraInfo}<br>`;
                 }
             }
