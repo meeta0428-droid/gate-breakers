94800f25ad30397528ddef28c754e80a97a90ea2
川井太一 <kawaitaichi@kawaitaichinoMacBook-Air.local>
2026-08-30 12:55:47 +0900

    Change Shinsoku Ryouiki effect to be voided on use

--- app_v6.js
+++ app_v6.js
@@ -1,4 +1,4 @@
-import { Character, calculateDamageFromCards, calculateDefenseFromCards, executeCardEffects, triggerHook } from './game_logic_v9.js?v=348';
+import { Character, calculateDamageFromCards, calculateDefenseFromCards, executeCardEffects, triggerHook } from './game_logic_v9.js?v=349';
 
 let cardPool = [];
 let player = null;
