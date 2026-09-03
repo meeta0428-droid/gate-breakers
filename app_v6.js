9035e1ec01c55d80dc7e18e72a9b36ebfc224ba4
川井太一 <kawaitaichi@kawaitaichinoMacBook-Air.local>
2026-09-04 01:56:28 +0900

    Fix blank pages appearing on even pages when printing

--- app_v6.js
+++ app_v6.js
@@ -1,4 +1,4 @@
-import { Character, calculateDamageFromCards, calculateDefenseFromCards, executeCardEffects, triggerHook } from './game_logic_v9.js?v=369';
+import { Character, calculateDamageFromCards, calculateDefenseFromCards, executeCardEffects, triggerHook } from './game_logic_v9.js?v=370';
 
 let cardPool = [];
 let player = null;
