7495647b161fc4725333d05a023bb8c5abe5c61d
川井太一 <kawaitaichi@kawaitaichinoMacBook-Air.local>
2026-09-06 04:06:44 +0900

    Change 機工士 cost from 4 to 6

--- app_v6.js
+++ app_v6.js
@@ -1,4 +1,4 @@
-import { Character, calculateDamageFromCards, calculateDefenseFromCards, executeCardEffects, triggerHook } from './game_logic_v9.js?v=386';
+import { Character, calculateDamageFromCards, calculateDefenseFromCards, executeCardEffects, triggerHook } from './game_logic_v9.js?v=387';
 
 let cardPool = [];
 let player = null;
