3e67a6f528bcc688a4d8f374eded69be282ddc66
川井太一 <kawaitaichi@kawaitaichinoMacBook-Air.local>
2026-09-06 04:11:12 +0900

    Change ディストラクトオーダー cost to 6

--- app_v6.js
+++ app_v6.js
@@ -1,4 +1,4 @@
-import { Character, calculateDamageFromCards, calculateDefenseFromCards, executeCardEffects, triggerHook } from './game_logic_v9.js?v=389';
+import { Character, calculateDamageFromCards, calculateDefenseFromCards, executeCardEffects, triggerHook } from './game_logic_v9.js?v=390';
 
 let cardPool = [];
 let player = null;
