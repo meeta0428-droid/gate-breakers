65b5e97bf61945b7929b54b62466763daad911aa
川井太一 <kawaitaichi@kawaitaichinoMacBook-Air.local>
2026-09-07 08:03:59 +0900

    Increase hand card size and text display length to improve readability

--- app_v6.js
+++ app_v6.js
@@ -1,4 +1,4 @@
-import { Character, calculateDamageFromCards, calculateDefenseFromCards, executeCardEffects, triggerHook } from './game_logic_v9.js?v=399';
+import { Character, calculateDamageFromCards, calculateDefenseFromCards, executeCardEffects, triggerHook } from './game_logic_v9.js?v=400';
 
 let cardPool = [];
 let player = null;
