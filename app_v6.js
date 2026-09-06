2b623d84b9e487fa1787b3d9367654ca4172b8dd
川井太一 <kawaitaichi@kawaitaichinoMacBook-Air.local>
2026-09-07 08:20:29 +0900

    Optimize layout by fixing hand area height and allowing central field area to take up all remaining screen space

--- app_v6.js
+++ app_v6.js
@@ -1,4 +1,4 @@
-import { Character, calculateDamageFromCards, calculateDefenseFromCards, executeCardEffects, triggerHook } from './game_logic_v9.js?v=400';
+import { Character, calculateDamageFromCards, calculateDefenseFromCards, executeCardEffects, triggerHook } from './game_logic_v9.js?v=401';
 
 let cardPool = [];
 let player = null;
