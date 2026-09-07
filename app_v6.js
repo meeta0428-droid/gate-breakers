2e77cc59e055b2d932eb9ffb36bbe639c75a0807
川井太一 <kawaitaichi@kawaitaichinoMacBook-Air.local>
2026-09-07 11:04:47 +0900

    Add apple-touch-icon and web app manifest for smartphone home screen icon

--- app_v6.js
+++ app_v6.js
@@ -1,4 +1,4 @@
-import { Character, calculateDamageFromCards, calculateDefenseFromCards, executeCardEffects, triggerHook } from './game_logic_v9.js?v=401';
+import { Character, calculateDamageFromCards, calculateDefenseFromCards, executeCardEffects, triggerHook } from './game_logic_v9.js?v=402';
 
 let cardPool = [];
 let player = null;
